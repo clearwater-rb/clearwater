5ec7f9bfbd443ae6bc90880fd75f1e598d38fbb9
Jamie Gaskins <jgaskins@gmail.com>
2015-02-22 14:04:22 -0500

    Add initial Component implementation
    A lot of web development is moving toward components. With Clearwater,
    I want to be able to use components somewhat interchangeably with
    controller/view stuff.
    
    This component class was extracted out of an app I've been playing with
    that lends *very* well to the component architecture over views and
    controllers. There are a couple of known issues:
    
    - Event handling
      - With views, events are placed on the body and scoped down to the
        selector contained within the view. For a lot of apps, this isn't a
        problem because it takes several hundred of these events to become
        noticeable at all on a reasonably fast computer.
      - With components, since there are more likely to be dozens of them on
        the screen at once, all these events become a problem much more
        quickly. Components are meant to be lightweight and disposable, so
        when you navigate away and come back, and a brand-new component is
        rendered, a new event is added.
    - Bindings
      - This commit basically replicates the way model bindings work on the
        component, but I think it improves it some.
      - The bindings here are removed if the element is no longer on the
        page. This fixes a memory leak caused by rerendering bound blocks.

---
+++ opal/clearwater/component.rb
@@ -0,0 +1,135 @@
+module Clearwater
+  class Component
+    attr_reader :options
+
+    def self.template template_name
+      @template_name = template_name
+    end
+
+    def self.template_name
+      @template_name
+    end
+
+    def self.attributes attributes
+      Array(attributes).each do |attr|
+        attr_reader attr
+
+        define_method "#{attr}=" do |new_value|
+          instance_variable_set "@#{attr}", new_value
+          @bindings[attr].each(&:call)
+          @bindings[attr].delete_if(&:dead?)
+        end
+      end
+    end
+
+    def initialize options={}
+      @options = options
+      @template = Template[self.class.template_name]
+      raise "No template for #{self.class}" unless @template
+
+      @tag_name = 'div'
+      @class_name = 'clearwater-component'
+
+      @bindings = Hash.new { |h, k| h[k] = [] }
+    end
+
+    def event event_type, target_selector, &block
+      document_body.on event_type, "##{element_id} #{target_selector}", &block
+    end
+
+    def render
+      element.html = to_html
+    end
+
+    def to_html
+      "<#@tag_name id=#{element_id.inspect} class=#{@class_name.inspect}>#{inner_html}</#@tag_name>"
+    end
+
+    def to_s
+      to_html
+    end
+
+    def inner_html
+      @template.render(self)
+    end
+
+    def bind model, property, &block
+      model.add_binding property, self, &block
+      "<span id='model-#{model.object_id}-#{property}'>#{yield}</span>"
+    end
+
+    def bind_attribute attribute, &block
+      binding = Binding.new(attribute, self, block)
+      @bindings[attribute].delete_if(&:dead?)
+      @bindings[attribute] << binding
+      binding.to_html
+    end
+
+    def remove_binding attribute, binding
+      binding_set = @bindings[attribute]
+      puts "#{binding_set.inspect}#delete #{binding.inspect}"
+      binding_set.delete binding
+      puts "Deleted"
+    end
+
+    def element_id
+      "#{self.class.name}-#{object_id}"
+    end
+
+    def element
+      document_body.find("##{element_id}")
+    end
+
+    def document_body
+      Element[`document.body`]
+    end
+
+    def method_missing *args, &block
+      if args.one? && @options.key?(args.first) && !block_given?
+        options[args.first]
+      else
+        raise NoMethodError, "no method #{args.shift}(#{args.join(',')}) for #{self.class}"
+      end
+    end
+
+    class Binding
+      def initialize attribute, component, block
+        @attribute = attribute
+        @component = component
+        @block = block
+        @dead = false
+      end
+
+      def call
+        element = Element[selector]
+        html = @block.call
+        if element.any?
+          element.html = html
+        else
+          dead!
+        end
+      end
+
+      def selector
+        "##{id}"
+      end
+
+      def id
+        "#{@component.class}-#{@component.object_id}-binding-#{object_id}-#{@attribute}"
+      end
+
+      def to_html
+        "<span id=#{id.inspect}>#{@block.call}</span>"
+      end
+
+      def dead?
+        @dead
+      end
+
+      def dead!
+        @dead = true
+      end
+    end
+  end
+end
+
