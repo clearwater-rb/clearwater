0e2229677c3ae37dd53dbb4a29b9131ba7fa0b23
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-04-15 15:33:51 -0500

    Switching to a better practice

--- lib/clearwater.rb
+++ lib/clearwater.rb
@@ -1,5 +1,8 @@
-require "clearwater/version"
 require "opal"
 require "opal/jquery"
+
+module Clearwater
+  require_relative "clearwater/version"
+end
 
 Opal.append_path(File.expand_path(File.join("..", "..", "opal"), __FILE__).untaint)
