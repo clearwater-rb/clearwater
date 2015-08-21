54f07e3196485350e840454d6319c822a8f43360
Jamie Gaskins <jgaskins@gmail.com>
2015-08-21 09:14:48 -0400

    Remove VirtualDOM::Element and VirtualDOM::Node
    The wrappers around the objects that actually meant something became so
    thin we were basically not even using them. Removing all those extra
    object allocations should streamline things. If we end up needing more
    functionality from the virtual-dom library, I'll add them back in.

--- opal/clearwater/virtual_dom.rb
+++ opal/clearwater/virtual_dom.rb
@@ -1,37 +1,19 @@
 require 'browser'
 require 'clearwater/virtual_dom/js/virtual_dom.js'
 
-module StringUtils
-  def self.camelize string
-    string.gsub(/_(\w)/) { |match| match.gsub(/_/, '').upcase }
-  end
-end
-
-module HashUtils
-  def self.camelize_keys(hash)
-    return hash unless hash.is_a? Hash
-
-    camelized = {}
-    hash.each do |k, v|
-      key = StringUtils.camelize(k)
-      value = if v.class == Hash
-                camelize_keys(v)
-              else
-                v
-              end
-
-      camelized[key] = value
-    end
-
-    camelized
-  end
-end
-
 module VirtualDOM
   def self.node(tag_name, attributes, content)
     content = sanitize_content(content)
     attributes = HashUtils.camelize_keys(attributes).to_n
-    Node.new(`virtualDom.h(tag_name, attributes, content)`)
+    `virtualDom.h(tag_name, attributes, content)`
+  end
+
+  def self.diff first, second
+    `virtualDom.diff(first, second)`
+  end
+
+  def self.patch node, diff
+    `virtualDom.patch(node, diff)`
   end
 
   def self.sanitize_content content
@@ -39,7 +21,6 @@ module VirtualDOM
       if(content === Opal.nil) return Opal.nil;
       if(content.$$class === Opal.Array)
         return #{content.map!{ |c| sanitize_content c }};
-      if(content.$$class === Opal.VirtualDOM.Node) return content.node;
       return content;
     }
   end
@@ -51,19 +32,19 @@ module VirtualDOM
 
     def render node
       if rendered?
-        diff = @node.diff(node)
-        @tree.patch diff
+        diff = VirtualDOM.diff @node, node
+        VirtualDOM.patch @tree, diff
         @node = node
       else
         @node = node
-        @tree = Element.new(create_element(node))
-        @root.inner_dom = @tree.to_n
+        @tree = create_element(node)
+        @root.inner_dom = @tree
         @rendered = true
       end
     end
 
     def create_element node
-      `virtualDom.create(node.node)`
+      `virtualDom.create(node)`
     end
 
     def rendered?
@@ -71,27 +52,29 @@ module VirtualDOM
     end
   end
 
-  class Node
-    def initialize(node)
-      @node = node
-    end
-
-    def diff other
-      `virtualDom.diff(self.node, other.node)`
+  module StringUtils
+    def self.camelize string
+      string.gsub(/_(\w)/) { |match| match.gsub(/_/, '').upcase }
     end
   end
 
-  class Element
-    def initialize(element)
-      @element = element
-    end
+  module HashUtils
+    def self.camelize_keys(hash)
+      return hash unless hash.is_a? Hash
 
-    def patch(diff)
-      `virtualDom.patch(#{to_n}, diff)`
-    end
+      camelized = {}
+      hash.each do |k, v|
+        key = StringUtils.camelize(k)
+        value = if v.class == Hash
+                  camelize_keys(v)
+                else
+                  v
+                end
 
-    def to_n
-      @element
+        camelized[key] = value
+      end
+
+      camelized
     end
   end
 end
