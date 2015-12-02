74c85a800de0ab8ae61ce39f851afea9ff7596ea
Jamie Gaskins <jgaskins@gmail.com>
2015-12-01 23:54:38 -0500

    Use Component.sanitize in SVGComponent
    This removes the duplication that happened in creating SVGComponent

--- opal/clearwater/svg_component.rb
+++ opal/clearwater/svg_component.rb
@@ -1,3 +1,4 @@
+require 'clearwater/component'
 require 'clearwater/virtual_dom'
 
 module Clearwater
@@ -102,58 +103,9 @@ module Clearwater
 
       VirtualDOM.svg(
         tag_name,
-        sanitize_attributes(attributes),
-        sanitize_content(content),
+        Component.sanitize_attributes(attributes),
+        Component.sanitize_content(content),
       )
-    end
-
-    def sanitize_attributes attributes
-      return attributes unless attributes.is_a? Hash
-
-      # Allow specifying `class` instead of `class_name`.
-      # Note: `class_name` is still allowed
-      if attributes.key? :class
-        if attributes.key? :class_name
-          warn "You have both `class` and `class_name` attributes for this " +
-            "element. `class` takes precedence: #{attributes}"
-        end
-
-        attributes[:class_name] = attributes.delete(:class)
-      end
-
-      attributes.each do |key, handler|
-        if key[0, 2] == 'on'
-          attributes[key] = proc do |event|
-            handler.call(Browser::Event.new(event))
-          end
-        end
-      end
-
-      attributes
-    end
-
-    def sanitize_content content
-      %x{
-        if(content && content.$$class) {
-          if(content.$$class === Opal.Array) {
-            return #{content.map { |c| `self.$sanitize_content(c)` }};
-          } else if(content === Opal.nil) {
-            return '';
-          } else {
-            var render = content.$render;
-
-            if(content.type === 'Thunk' && typeof(content.render) === 'function') {
-              return content;
-            } else if(render && !render.$$stub) {
-              return self.$sanitize_content(content.$render());
-            } else {
-              return content;
-            }
-          }
-        } else {
-          return content;
-        }
-      }
     end
   end
 end
