e40b0e898a4d7fee90bd9c176ae77da776e1108a
Jamie Gaskins <jgaskins@gmail.com>
2016-06-15 23:51:30 -0400

    Add camelization spec

---
+++ spec-opal/clearwater/virtual_dom/string_utils_spec.rb
@@ -0,0 +1,17 @@
+require 'clearwater/virtual_dom'
+
+module VirtualDOM
+  describe StringUtils do
+    describe :camelize do
+      {
+        'foo' => 'foo',
+        'foo_bar' => 'fooBar',
+        'foo_bar_baz' => 'fooBarBaz',
+      }.each do |source, target|
+        it 'camelizes a string' do
+          expect(StringUtils.camelize(source)).to eq target
+        end
+      end
+    end
+  end
+end
