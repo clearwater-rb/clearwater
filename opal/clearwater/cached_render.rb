04ac7ff22c052ead1f2bef08fbbee13763cf14f0
Jamie Gaskins <jgaskins@gmail.com>
2016-07-24 12:14:24 -0400

    Add CachedRender wrapper
    This keeps the component from being injected with virtual-DOM logic. It
    can simply be its own Ruby object. The wrapper acts as an adapter
    between the two.

--- opal/clearwater/cached_render.rb
+++ opal/clearwater/cached_render.rb
@@ -4,27 +4,16 @@ module Clearwater
   module CachedRender
     def self.included base
       %x{
-        Opal.defn(self, 'type', 'Thunk');
-        Opal.defn(self, 'render', function(prev) {
-          var self = this;
-
-          if(prev && prev.vnode && #{!should_render?(`prev`)}) {
-            return prev.vnode;
-          } else {
-            var content = #{Component.sanitize_content(render)};
-
-            while(content && content.type == 'Thunk' && content.render) {
-              content = #{Component.sanitize_content(`content.render(prev)`)};
-            }
-
-            return content;
-          }
-        });
+        Opal.defn(self, '$$cached_render', true);
       }
     end
 
     def should_render? _
       false
+    end
+
+    def key
+      `undefined`
     end
   end
 end
