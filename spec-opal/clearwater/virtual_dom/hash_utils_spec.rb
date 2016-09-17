clearwater-rb/clearwater

commit 18c331a69e7cf15fb8e4fd6471b04a066b5d640d

Optimize virtual-DOM generation
Coalescing sanitizing virtual-DOM attributes and conversion into a
native JS object removes the object allocations that happen with
creating a new hash.

On a fast-rendering app, this improved framerate from 37-43 fps. It
intentionally doesn't run at 60fps specifically so performance
improvements are easier to measure. :-)

opal/clearwater/virtual_dom.rb
@@ -2,16 +2,20 @@ require 'clearwater/virtual_dom/js/virtual_dom.js'
 
 module VirtualDOM
   def self.node(tag_name, attributes=nil, content=nil)
-    content = sanitize_content(content)
-    attributes = HashUtils.camelize_keys(attributes).to_n
-    `virtualDom.h(tag_name, attributes, content)`
+    %x{
+      return virtualDom.h(
+        tag_name,
+        #{HashUtils.camelized_native(attributes)},
+        #{sanitize_content(content)}
+      );
+    }
   end
 
   def self.svg(tag_name, attributes=nil, content=nil)
     %x{
       return virtualDom.svg(
         tag_name,
-        #{HashUtils.camelize_keys(attributes).to_n},
+        #{HashUtils.camelized_native(attributes)},
         #{sanitize_content(content)}
       );
     }
@@ -67,31 +71,21 @@ module VirtualDOM
 
   module StringUtils
     def self.camelize string
-      %x{
-        return string.replace(/_(\w)/g, function(full_match, character_match) {
-          return character_match.toUpperCase();
-        })
-      }
+      `string.replace(/_(\w)/g, self.$_camelize_handler)`
+    end
+
+    def self._camelize_handler _, character_match
+      `character_match.toUpperCase()`
     end
   end
 
   module HashUtils
-    def self.camelize_keys(hash)
-      return hash unless hash.is_a? Hash
+    def self.camelized_native hash
+      return hash.to_n unless `!!hash.$$is_hash`
 
-      camelized = {}
-      hash.each do |k, v|
-        key = StringUtils.camelize(k)
-        value = if v.class == Hash
-                  camelize_keys(v)
-                else
-                  v
-                end
-
-        camelized[key] = value
+      hash.each_with_object(`{}`) do |(k, v), js_obj|
+        `js_obj[#{StringUtils.camelize(k)}] = v.$$is_hash ? self.$camelized_native(v) : v`
       end
-
-      camelized
     end
   end
 end

opal/clearwater/component.rb
@@ -4,6 +4,63 @@ module Clearwater
   module Component
     attr_accessor :router, :outlet
 
+    def params
+      router.params
+    end
+
+    def self.sanitize_attributes attributes
+      return attributes unless attributes.is_a? Hash
+
+      attributes.each do |key, value|
+        if `key.slice(0, 2)` == 'on'
+          attributes[key] = proc do |event|
+            value.call(Bowser::Event.new(event))
+          end
+        end
+      end
+
+      # Allow specifying `class` instead of `class_name`.
+      # Note: `class_name` is still allowed
+      if attributes.key?(:class)
+        if attributes.key?(:class_name)
+          warn "You have both `class` and `class_name` attributes for this " +
+            "element. `class` takes precedence: #{attributes}"
+        end
+
+        attributes[:class_name] = attributes.delete :class
+      end
+
+      attributes
+    end
+
+    def self.sanitize_content content
+      %x{
+        if(content && content.$$class) {
+          if(content.$$is_array) {
+            return #{content.map { |c| sanitize_content(c) }};
+          } else {
+            var render = content.$render;
+
+            if(content.type === 'Thunk' && typeof(content.render) === 'function') {
+              return content;
+            } else if(render && !render.$$stub) {
+              return self.$sanitize_content(content.$render());
+            } else {
+              return content;
+            }
+          }
+        } else {
+          return content;
+        }
+      }
+    end
+
+    # Default render method for stubbing
+    def render
+    end
+
+    module_function
+
     HTML_TAGS = %w(
       a
       abbr
@@ -118,75 +175,20 @@ module Clearwater
       wbr
     )
 
-    def params
-      router.params
-    end
-
-    def self.sanitize_attributes attributes
-      return attributes unless attributes.is_a? Hash
-
-      # Allow specifying `class` instead of `class_name`.
-      # Note: `class_name` is still allowed
-      if attributes.key? :class
-        if attributes.key? :class_name
-          warn "You have both `class` and `class_name` attributes for this " +
-            "element. `class` takes precedence: #{attributes}"
-        end
-
-        attributes[:class_name] = attributes.delete(:class)
-      end
-
-      attributes.each do |key, handler|
-        if key[0, 2] == 'on'
-          attributes[key] = proc do |event|
-            handler.call(Bowser::Event.new(event))
-          end
-        end
-      end
-
-      attributes
-    end
-
-    def self.sanitize_content content
-      %x{
-        if(content && content.$$class) {
-          if(content.$$class === Opal.Array) {
-            return #{content.map { |c| `self.$sanitize_content(c)` }};
-          } else {
-            var render = content.$render;
-
-            if(content.type === 'Thunk' && typeof(content.render) === 'function') {
-              return content;
-            } else if(render && !render.$$stub) {
-              return self.$sanitize_content(content.$render());
-            } else {
-              return content;
-            }
-          }
-        } else {
-          return content;
-        }
-      }
-    end
-
-    # Default render method for stubbing
-    def render
-    end
-
-    module_function
-
     HTML_TAGS.each do |tag_name|
       define_method(tag_name) do |attributes, content|
+        %x{
+          if(!(attributes === nil || attributes.$$is_hash)) {
+            content = attributes;
+            attributes = nil;
+          }
+        }
+
         tag(tag_name, attributes, content)
       end
     end
 
     def tag tag_name, attributes=nil, content=nil
-      if !(`attributes.$$is_hash || attributes === #{nil}`)
-        content = attributes
-        attributes = nil
-      end
-
       VirtualDOM.node(
         tag_name,
         Component.sanitize_attributes(attributes),

commit ba76051e14a4071a84b06fcd1eb85506652da368

Nativize attribute values in VirtualDOM
This fixes a regression in 18c331a69e7cf15fb8e4fd6471b04a066b5d640d.
Since we no longer create the intermediate hash, we also stopped calling
to_n on the attributes.

This commit also properly namespaces the VirtualDOM component inside the
Clearwater module.

opal/clearwater/virtual_dom.rb
@@ -1,107 +1,115 @@
 require 'clearwater/virtual_dom/js/virtual_dom.js'
 
-module VirtualDOM
-  def self.node(tag_name, attributes=nil, content=nil)
-    %x{
-      return virtualDom.h(
-        tag_name,
-        #{HashUtils.camelized_native(attributes)},
-        #{sanitize_content(content)}
-      );
-    }
-  end
-
-  def self.svg(tag_name, attributes=nil, content=nil)
-    %x{
-      return virtualDom.svg(
-        tag_name,
-        #{HashUtils.camelized_native(attributes)},
-        #{sanitize_content(content)}
-      );
-    }
-  end
-
-  def self.create_element(node)
-    `virtualDom.create(node)`
-  end
-
-  def self.diff first, second
-    `virtualDom.diff(first, second)`
-  end
-
-  def self.patch node, diff
-    `virtualDom.patch(node, diff)`
-  end
-
-  def self.sanitize_content content
-    %x{
-      if(content === Opal.nil || content === undefined) return null;
-      if(content.$$is_array)
-        return #{content.map!{ |c| sanitize_content c }};
-      return content;
-    }
-  end
-
-  class Document
-    def initialize(root=Bowser.document.create_element('div'))
-      @root = root
+module Clearwater
+  module VirtualDOM
+    def self.node(tag_name, attributes=nil, content=nil)
+      %x{
+        return virtualDom.h(
+          tag_name,
+          #{HashUtils.camelized_native(attributes)},
+          #{sanitize_content(content)}
+        );
+      }
     end
 
-    def render node
-      if rendered?
-        diff = VirtualDOM.diff @node, node
-        VirtualDOM.patch @tree, diff
-        @node = node
-      else
-        @node = node
-        @tree = create_element(node)
-        @root.inner_dom = @tree
-        @rendered = true
-      end
+    def self.svg(tag_name, attributes=nil, content=nil)
+      %x{
+        return virtualDom.svg(
+          tag_name,
+          #{HashUtils.camelized_native(attributes)},
+          #{sanitize_content(content)}
+        );
+      }
     end
 
-    def create_element node
+    def self.create_element(node)
       `virtualDom.create(node)`
     end
 
-    def rendered?
-      @rendered
+    def self.diff first, second
+      `virtualDom.diff(first, second)`
     end
-  end
 
-  module StringUtils
-    # Speed up camelization like whoa.
-    %x{ var camelized_cache = {}; }
+    def self.patch node, diff
+      `virtualDom.patch(node, diff)`
+    end
 
-    def self.camelize string
+    def self.sanitize_content content
       %x{
-        if(camelized_cache.hasOwnProperty(string)) {
-          return camelized_cache[string];
-        } else {
-          camelized_cache[string] = string.replace(/_(\w)/g, self.$_camelize_handler);
-          return camelized_cache[string];
-        }
+        if(content === Opal.nil || content === undefined) return null;
+        if(content.$$is_array)
+          return #{content.map!{ |c| sanitize_content c }};
+        return content;
       }
     end
 
-    def self._camelize_handler _, character_match
-      `character_match.toUpperCase()`
+    class Document
+      def initialize(root=Bowser.document.create_element('div'))
+        @root = root
+      end
+
+      def render node
+        if rendered?
+          diff = VirtualDOM.diff @node, node
+          VirtualDOM.patch @tree, diff
+          @node = node
+        else
+          @node = node
+          @tree = create_element(node)
+          @root.inner_dom = @tree
+          @rendered = true
+        end
+      end
+
+      def create_element node
+        `virtualDom.create(node)`
+      end
+
+      def rendered?
+        @rendered
+      end
     end
-  end
 
-  module HashUtils
-    def self.camelized_native hash
-      return hash.to_n unless `!!hash.$$is_hash`
+    module StringUtils
+      # Speed up camelization like whoa.
+      %x{ var camelized_cache = {}; }
 
-      %x{
-        var v, keys = #{hash.keys}, key, js_obj = {};
-        for(var index = 0; index < keys.length; index++) {
-          key = keys[index];
-          v = #{hash[`key`]};
-          js_obj[#{StringUtils.camelize(`key`)}] = v.$$is_hash ? self.$camelized_native(v) : v
+      def self.camelize string
+        %x{
+          if(camelized_cache.hasOwnProperty(string)) {
+            return camelized_cache[string];
+          } else {
+            camelized_cache[string] = string.replace(/_(\w)/g, self.$_camelize_handler);
+            return camelized_cache[string];
+          }
         }
-        return js_obj;
-      }
+      end
+
+      def self._camelize_handler _, character_match
+        `character_match.toUpperCase()`
+      end
+    end
+
+    module HashUtils
+      def self.camelized_native hash
+        return hash.to_n unless `!!hash.$$is_hash`
+
+        %x{
+          var v, keys = #{hash.keys}, key, js_obj = {};
+          for(var index = 0; index < keys.length; index++) {
+            key = keys[index];
+            v = #{hash[`key`]};
+            js_obj[#{StringUtils.camelize(`key`)}] = v.$$is_hash
+              ? self.$camelized_native(v)
+              : (
+                (v && v.$$class) // If this is a Ruby object, nativize it
+                  ? #{`v`.to_n}
+                  : v
+                );
+          }
+          return js_obj;
+        }
+      end
     end
   end
 end

spec-opal/clearwater/virtual_dom/hash_utils_spec.rb
@@ -1,0 +1,30 @@
+require 'clearwater/virtual_dom'
+
+module Clearwater
+  module VirtualDOM
+    describe HashUtils do
+      let(:hash) do
+        {
+          src: 'foo',
+          omg: nil,
+          style: {
+            background_color: :blue,
+          },
+        }
+      end
+      let(:native) { HashUtils.camelized_native(hash) }
+
+      it 'uses string keys normally' do
+        expect(`#{native}.src === 'foo'`).to be_truthy
+      end
+
+      it 'converts nil keys to null' do
+        expect(`#{native}.omg == null`).to be_truthy
+      end
+
+      it 'camelizes sub-hash keys' do
+        expect(`#{native}.style.backgroundColor === 'blue'`).to be_truthy
+      end
+    end
+  end
+end

spec-opal/clearwater/virtual_dom/string_utils_spec.rb
@@ -1,15 +1,17 @@
 require 'clearwater/virtual_dom'
 
-module VirtualDOM
-  describe StringUtils do
-    describe :camelize do
-      {
-        'foo' => 'foo',
-        'foo_bar' => 'fooBar',
-        'foo_bar_baz' => 'fooBarBaz',
-      }.each do |source, target|
-        it 'camelizes a string' do
-          expect(StringUtils.camelize(source)).to eq target
+module Clearwater
+  module VirtualDOM
+    describe StringUtils do
+      describe :camelize do
+        {
+          'foo' => 'foo',
+          'foo_bar' => 'fooBar',
+          'foo_bar_baz' => 'fooBarBaz',
+        }.each do |source, target|
+          it 'camelizes a string' do
+            expect(StringUtils.camelize(source)).to eq target
+          end
         end
       end
     end