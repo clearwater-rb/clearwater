32cc5289794fed4f8cddfadc67c6b59920dcae48
Jamie Gaskins <jgaskins@gmail.com>
2017-09-17 02:01:39 -0400

    Add AsyncRender component (#72)
    * Add AsyncRender
    
    * Rename AsyncRender file
    
    Oops.
    
    * Render async using blocks
    
    This fixes accidental eager sanitization when using the following:
    
        div(async_render { MyAsyncComponent.new })

---
+++ opal/clearwater/async_render.rb
@@ -0,0 +1,47 @@
+require 'clearwater/virtual_dom'
+require 'clearwater/black_box_node'
+
+module Clearwater
+  class AsyncRender
+    include Clearwater::BlackBoxNode
+
+    attr_reader :vnode
+
+    def initialize &content
+      if content.is_a? Array
+        warn "#{self.class} cannot operate on an Array, so it will use the first element. If you need to use an array, you can wrap it inside another element."
+        content = content.first
+      end
+
+      @content = content
+    end
+
+    # Specify a temporary node to use. It will be replaced with the content
+    # passed in.
+    def node
+      Component.span
+    end
+
+    def mount element
+      delayed_update nil, element
+    end
+
+    def update previous, element
+      delayed_update previous.vnode, element
+    end
+
+    def delayed_update from, element
+      Bowser.window.animation_frame do
+        @vnode = Component.sanitize_content(@content.call)
+        diff = VirtualDOM.diff from, @vnode
+        VirtualDOM.patch element.to_n, diff
+      end
+    end
+  end
+
+  module Component
+    def async_render &block
+      AsyncRender.new &block
+    end
+  end
+end
